4eb67d00cab7caabd1b4172ca078ad141288d9d2
devaaronmcintyre <amcintyre2@mail.csuchico.edu>
2014-05-04 02:08:34 -0700

    Finished algorithm
    Gaussian Elimination with partial pivoting. L2norm is returning NAN.
    Ready to do timing runs.

--- finalproject/omp_elimination.c
+++ finalproject/omp_elimination.c
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>   
+#include <sys/time.h>
 #include <time.h>
 #include <math.h>
 
@@ -23,7 +24,7 @@
  				 double* array: pointer to that will point to the result of this function
  * Return val:   None
  */
-void generate_random_matrix_vector_p(int* n, double* A, double* b);
+void generate_random_matrix_vector_p(int* n, double* A, double* b, int* thread_count);
 
 /*------------------------------------------------------------------
  * Function:     print_matrix
@@ -35,12 +36,23 @@ void generate_random_matrix_vector_p(int* n, double* A, double* b);
  */
 void print_matrix(int* n, int* m, double* arr);
 
-void ppivot(int* j, int* n, double* A, double* b);
+void ppivot(int* j, int* n, double* A, double* b, int* thread_count);
 
+void elimination(int* j, int* n, double* A, double* b, int* thread_count);
+
+void back_substitution(int* n, double* A, double* b, double* x, int* thread_count);
+
+void l2norm(int* n, double* A, double* b, double* x);
+
+double time_to_double(struct timeval *timevalue);
 /*--------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
 	double* A; //matrix A in row major form
 	double* b; //vector b size of n
+	double* x; //results vector x size of n
+	struct timeval tstart, tfinish;
+	struct timeval tdiff;
+
 	
 	//size of matrix
 	int n = strtol(argv[1], NULL, 10);
@@ -54,25 +66,109 @@ int main(int argc, char* argv[]) {
 	//allocate space for the matrix A and vector b
 	A = (double*) malloc(matrix_arr_sz);
 	b = (double*) malloc(vector_arr_sz);
+	x = (double*) malloc(vector_arr_sz);
 	
-	generate_random_matrix_vector_p(&n, A, b);
+	generate_random_matrix_vector_p(&n, A, b, &thread_count);
 
  
-	print_matrix(&n, &n, A);
-	print_matrix(NULL, &n, b);
+	//print_matrix(&n, &n, A);
+	//print_matrix(NULL, &n, b);
 	int j;
+	//start timing
+	gettimeofday(&tstart, NULL);
 	for(j=0; j <n-1; ++j){
-		ppivot(&j, &n, A, b);
+		ppivot(&j, &n, A, b, &thread_count);
+		elimination(&j, &n, A, b, &thread_count);
 	}
-	print_matrix(&n, &n, A);
-	print_matrix(NULL, &n, b);
-
-
-
+	back_substitution(&n, A, b, x, &thread_count);
+	//stop timing.
+	gettimeofday(&tfinish, NULL);
+	l2norm(&n, A, b, x);
+	timersub(&tfinish, &tstart, &tdiff);
+	double elapsed = time_to_double(&tdiff);
+	printf("Elapsed Time %fms\n", elapsed);
+	//print_matrix(&n, &n, A);
+	//print_matrix(NULL, &n, b);
+	//print_matrix(NULL, &n, x);
+	free(A);
+	A = NULL;
+	free(b);
+	b = NULL;
+	free(x);
+	x = NULL;
 	return 0; 
 }  /* main */
 
-void ppivot(int* j, int* n, double* A, double* b){
+double time_to_double(struct timeval *timevalue)
+{
+	double result;
+	result = timevalue->tv_sec * 1000.0;      // sec to ms
+	result += timevalue->tv_usec / 1000.0;   // us to ms
+	return result;
+}
+
+void l2norm(int* n, double* A, double* b, double* x){
+	double* residual = malloc((*n)*sizeof(double));
+	double sum_of_sqrs;
+	int i, j;
+	for (i = 0; i < (*n); ++i)
+	{
+		residual[i] = 0.0;
+		for(j = i; j < (*n); ++j)
+		{
+			residual[i] += A[i*(*n)+j] * x[j];
+		}
+		//printf("before residual %f\n", residual[i]);
+		//printf("before b[i] = %f\n", b[i]);
+		residual[i] = fabs(residual[i] - b[i]);
+		//printf("after residual %f\n", residual[i]);
+		sum_of_sqrs += pow(residual[i], 2);
+	}
+	printf("l2norm =%f %f\n", sum_of_sqrs, sqrt(sum_of_sqrs));
+	free(residual);
+	residual = NULL;
+}
+
+void elimination(int* j, int* n, double* A, double* b, int* thread_count){
+	int i, k;
+	omp_set_nested(1); // 1 - enables nested parallelism; 0 - disables nested parallelism.
+	#pragma omp parallel for private(i) num_threads(*thread_count)
+		for(i=(*j)+1; i < (*n); ++i)
+		{
+			double multiplier = A[i*(*n) + (*j)]/A[(*j)*(*n)+(*j)];
+			double temp = 0.0;
+			#pragma omp parallel for shared(multiplier) private(k) num_threads(*thread_count)
+				for (k = 0; k < (*n); ++k)
+				{
+					temp = multiplier * A[(*j)*(*n)+k];
+					A[i*(*n)+k] = A[i*(*n)+k] - temp;
+				}
+			double btemp = multiplier * b[i];
+			b[i] = b[i] - btemp;
+
+		}
+}
+
+void back_substitution(int* n, double* A, double* b, double* x, int* thread_count){
+	int i, j;
+	double sum;
+	for(i = (*n)-1; i>=0; --i){
+		sum = 0.0;
+		#pragma omp parallel for private(j) reduction(+: sum) num_threads(*thread_count)
+			for (j = i+1; j < (*n); ++j){
+				//printf("A[i][j]:%f * x[j]:%f = %f\n", A[i*(*n)+j], x[j], A[i*(*n)+j]*x[j]);
+				sum += A[i*(*n)+j]*x[j];
+			}
+		//printf("sum row %d is %f\n", i, sum);
+		x[i] = b[i]-sum;
+		//printf("%f\n", x[i]);
+		x[i] = x[i]/A[i*(*n)+i];
+		//printf("x =%f\n", x[i]);
+	}
+
+}
+
+void ppivot(int* j, int* n, double* A, double* b, int* thread_count){
 
 	int maxrow = *j;
 	int lmaxrow;
@@ -80,7 +176,7 @@ void ppivot(int* j, int* n, double* A, double* b){
 	double lmax;
 	int i;
 
-	#pragma omp parallel private(lmax) private(lmaxrow) shared(max) private(i)
+	#pragma omp parallel private(lmax) private(lmaxrow) shared(max) private(i) num_threads(*thread_count)
 	{
 		lmax = 0;
 		lmaxrow = *j;
@@ -105,7 +201,7 @@ void ppivot(int* j, int* n, double* A, double* b){
 
 	//if max row is not current pivot row then swap rows.
 	if((*j) < maxrow){
-		#pragma omp parallel for private(i)
+		#pragma omp parallel for private(i) num_threads(*thread_count)
 			for (i = 0;i<(*n); ++i)
 			{
 				double temp = 0.0;
@@ -119,13 +215,11 @@ void ppivot(int* j, int* n, double* A, double* b){
 		b[(*j)] = b[maxrow];
 		b[maxrow] = btemp;
 	}
-	printf("maxrow %d, value %f\n", maxrow, max);
 }
-void generate_random_matrix_vector_p(int* n, double* A, double* b){
+void generate_random_matrix_vector_p(int* n, double* A, double* b, int* thread_count){
 //allocate an array of size n^2-1 * size of double to store a n*n matrix
-	#pragma omp parallel
+	#pragma omp parallel num_threads(*thread_count)
 	{
-		printf("n is %d\n", *n);
 		struct drand48_data randBuffer;
 		srand48_r((time(NULL) ^ omp_get_thread_num()), &randBuffer);
 	   	int i;
@@ -143,7 +237,6 @@ void generate_random_matrix_vector_p(int* n, double* A, double* b){
 	   		drand48_r(&randBuffer, &x);
 			double tmp = -1 + 2*x;
 				b[i] = tmp;
-				printf("%f\n", tmp);
 		}
 	}
 }
